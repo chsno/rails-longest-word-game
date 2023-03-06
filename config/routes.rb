06b54da5e72125fe0a1c4eae3d651e177976132e
Charles Snollaerts <csnollaerts@gmail.com>
2023-03-06 16:21:20 -0300

    3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get 'games/new'
-  post 'games/score'
+  get '/new', to: 'games#new'
+  post '/score', to: 'games#score'
 end
